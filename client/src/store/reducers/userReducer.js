bf2378434655fc7673a97fc7286e6e7a68b432a7
HuvonHutchinson-Goodridge <huvongoodridge@gmail.com>
2023-01-12 02:50:34 -0500

    created userReducer for storing all of the users

---
+++ client/src/store/reducers/userReducer.js
@@ -0,0 +1,12 @@
+INITIAL_STATE = [];
+
+//Reducer for storing all of the users in the database
+
+export const userReducer = (state = INITIAL_STATE, action) => {
+    switch (action.type) {
+        case "USERS":
+            return [...state, ...action.payload];
+        default:
+            return state
+    }
+}
