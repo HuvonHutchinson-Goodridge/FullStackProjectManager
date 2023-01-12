89460c48f7e417668bb43729ca1a82784ed53a2d
HuvonHutchinson-Goodridge <huvongoodridge@gmail.com>
2023-01-12 02:36:55 -0500

    changed name of file

---
+++ client/src/store/reducers/selectedProjectReducer.js
@@ -0,0 +1,19 @@
+const INITIAL_STATE = {
+    name: '',
+    numOfUsers: 0,
+    numOfBugs: 0,
+    bugsPending: 0,
+    bugsResolved: 0,
+    description: '',
+    image: '',
+    id: ''
+}
+
+export const selectedProjectReducer = (state = INITIAL_STATE, action) => {
+    switch (action.type) {
+        case 'PROJECT_SELECTED':
+            return { ...state, ...action.payload }
+        default:
+            return state
+    }
+}
