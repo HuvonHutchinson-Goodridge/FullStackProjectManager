0967927748f5dd05b1c7689d7387723cfa7bcf13
HuvonHutchinson-Goodridge <huvongoodridge@gmail.com>
2023-01-19 03:11:32 -0500

    added proper progress from project data model to progress Circle. removed unnecessary imports

--- client/src/components/dashboard/ProgressCircle.js
+++ client/src/components/dashboard/ProgressCircle.js
@@ -1,17 +1,30 @@
-import { Box, useTheme } from "@mui/material";
-import { tokens } from "./../../theme"
-const ProgressCircle = ({ progress = "0.75", size = "40" }) => {
-    const theme = useTheme();
-    const colors = tokens(theme.palette.mode);
-    const angle = progress * 360;
+import { Box, Typography } from "@mui/material";
+import * as React from 'react';
+import CircularProgress from '@mui/material/CircularProgress';
 
+const ProgressCircle = ({progress}) => {
     return (
-        <Box sx={{
-            background: `radial-gradient(${colors.primary[400]} 55%, transparent 56%),
-                    conic-gradient(transparent 0deg ${angle}deg, ${colors.blueAccent[500]} ${angle}def 360deg),
-                    ${colors.greenAccent[500]}`,
-            borderRadius: "50%", width: `${size}px`, height: `${size}px`
-        }}/>)
+        <Box sx={{position: 'relative', display: 'inline-flex'}}>
+            <CircularProgress color="secondary" variant="determinate" value={progress}/>
+            <Box
+                sx={{
+                    top: 0,
+                    left: 0,
+                    bottom: 0,
+                    right: 0,
+                    position: 'absolute',
+                    display: 'flex',
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                }}
+            >
+                <Typography variant="caption" component="div" color="text.secondary">
+                    {`${progress}%`}
+                </Typography>
+            </Box>
+        </Box>
+    )
 }
 
 export default ProgressCircle;
+
