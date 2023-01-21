a9359d927e04387a9d62ce9419911acf01b6e0ef
HuvonHutchinson-Goodridge <huvongoodridge@gmail.com>
2023-01-21 01:37:02 -0500

    added height styling, and removed increase prop.

--- client/src/components/dashboard/StatBox.js
+++ client/src/components/dashboard/StatBox.js
@@ -2,37 +2,34 @@ import { Box, Typography, useTheme } from "@mui/material";
 import { tokens } from "./../../theme";
 import ProgressCircle from "./ProgressCircle";
 
-const StatBox = ({ title, subtitle, icon, progress, increase }) => {
+const StatBox = ({ title, subtitle, icon, progress}) => {
     const theme = useTheme();
     const colors = tokens(theme.palette.mode);
 
     return (
-        <Box width="100%" m="0 30px">
-            <Box display="flex" justifyContent="space-between">
-                <Box>
-                    {icon}
+        <Box width="100%" borderRadius = "3px" height="100%" m="0 15px" p ="5px" backgroundColor={colors.primary[400]}>
+            <Box >
+                <Box display='flex' justifyContent="space-between">
+                    <Box display='flex' justifyContent="center" alignItems="center">
+                        {icon}
+                        <Typography
+                            variant="h5"
+                            fontWeight="bold"
+                            sx={{ color: colors.greenAccent[500] }}>
+                            {subtitle}
+                        </Typography>
+                    </Box>
+                    <Box>
+                        <ProgressCircle progress={progress} />
+                    </Box>
+                </Box>
+
+                <Box display="flex" justifyContent="space-between">
                     <Typography
                         variant="h4"
                         fontWeight="bold"
                         sx={{ color: colors.grey[100] }}>
                         {title}
-                    </Typography>
-                </Box>
-                <Box>
-                    <ProgressCircle progress={progress} />
-                </Box>
-                <Box display="flex" justifyContent="space-between">
-                    <Typography
-                        variant="h5"
-                        fontWeight="bold"
-                        sx={{ color: colors.greenAccent[500]}}>
-                        {subtitle}
-                    </Typography>
-                    <Typography
-                        variant="h5"
-                        fontStyle="italic"
-                        sx={{ color: colors.greenAccent[600] }}>
-                        {increase}
                     </Typography>
                 </Box></Box> </Box>)
 }
