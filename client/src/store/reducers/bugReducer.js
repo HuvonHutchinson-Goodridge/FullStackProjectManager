68abc2587445cf307f1221626ea1064c160a5b5c
HuvonHutchinson-Goodridge <huvongoodridge@gmail.com>
2023-01-22 17:41:56 -0500

    fixed error where state was adding all bugs again

--- client/src/store/reducers/bugReducer.js
+++ client/src/store/reducers/bugReducer.js
@@ -3,10 +3,24 @@ const INITIAL_STATE = [];
 /*Reducer for all bugs in the database
  * */
 export const bugReducer = (state = INITIAL_STATE, action) => {
-    switch (action.type) {
-        case "BUGS":
-            return [...state, ...action.payload]
-        default:
-            return state;
+    if (action.payload?.bugs != undefined) {
+        state.forEach((bugsOnProject, index, array) => {
+            if (bugsOnProject[0] != undefined && action.payload.projectID === bugsOnProject.project) {
+                array[index] = action.payload.bugs
+        }
+        }) 
+        switch (action.type) {
+            case "BUGS":
+                return [...state]
+            default:
+                return state;
+        }
+    } else {
+        switch (action.type) {
+            case "BUGS":
+                return [...state, ...action.payload]
+            default:
+                return state;
+        }
     }
 }
