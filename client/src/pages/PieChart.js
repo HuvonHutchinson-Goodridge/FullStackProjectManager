b3ba26c38067a35a33a1c7be01d09edef18d5305
HuvonHutchinson-Goodridge <huvongoodridge@gmail.com>
2023-01-23 23:19:26 -0500

    updated useEffect dependency

--- client/src/pages/PieChart.js
+++ client/src/pages/PieChart.js
@@ -7,7 +7,7 @@ import Pie from "./../components/Pie";
 const PieChart = ({ fetchPage }) => {
     useEffect(() => {
         fetchPage("PIE CHART", "PROJECT PROGRESS")
-    }, [])
+    }, [fetchPage])
     return (
         <Box m="20px" >
             <Grid md={12}>
