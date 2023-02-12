a456b821e8ef471c6e2b17e672e24b43b4f0b999
HuvonHutchinson-Goodridge <huvongoodridge@gmail.com>
2023-02-12 04:22:07 -0500

    removed unnecessary imports

--- client/src/pages/Team.js
+++ client/src/pages/Team.js
@@ -2,8 +2,7 @@ import { Box, Grid, Typography } from '@mui/material';
 import { DataGrid } from '@mui/x-data-grid';
 import { useTheme } from "@mui/material"
 import { tokens } from "./../theme"
-import { useEffect, useState } from 'react'
-import axios from 'axios'
+import { useEffect } from 'react'
 import AdminPanelSettingsOutlinedIcon from '@mui/icons-material/AdminPanelSettingsOutlined';
 import LockOpenOutlinedIcon from '@mui/icons-material/LockOpenOutlined';
 import { fetchPage } from "./../store/actions"
