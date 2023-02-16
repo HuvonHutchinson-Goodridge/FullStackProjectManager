9e6c3a5ffd3d8a62bf2f6c8edc9f080b892e367e
HuvonHutchinson-Goodridge <huvongoodridge@gmail.com>
2023-02-16 02:36:18 -0500

    removed unnecessary functions, imports, and middleware

--- server/controllers/bugController.js
+++ server/controllers/bugController.js
@@ -1,15 +1,5 @@
-const AppError = require("./../utils/appError");
 const Bug = require("./../models/bugModel")
-const APIFeatures = require("./../utils/apiFeatures")
-const catchAsync = require('./../utils/catchAsync');
 const factory = require('./handlerFactory')
-
-exports.aliasBestBugs = (request, response, next) => {
-    request.query.limit = '5';
-    request.query.sort = "numOfPeople";
-    request.query.fields = 'user,numOfPeople,bug'
-    next();
-}
 
 exports.setProjectUserIds = (request, response, next) => {
     //Allows for nested routes
@@ -24,35 +14,3 @@ exports.createBug = factory.createOne(Bug);
 exports.updateBug = factory.updateOne(Bug);
 exports.deleteBug = factory.deleteOne(Bug);
 
-exports.getBugStats = catchAsync(async (request, response, next) => {
-    const stats = await Bug.aggregate([
-        {
-            $match: {
-                numOfPeople: { $gte: 10 }
-            }
-        },
-        {
-            $group: {
-                _id: { $toUpper: "$bugStatus" },
-                avgNumOfPeople: { $avg: '$numOfPeople' },
-                numOfBugs: { $sum: 1 },
-                numOfPending: { $sum: 1 },
-                minPeople: { $min: "$numOfPeople" },
-                maxPeople: { $max: "numOfPeople" }
-            }
-        },
-        {
-            $sort: {
-                numOfPeople: 1
-            }
-        },
-        {
-            $match: { _id: { $ne: "Pending" } }
-        }
-    ])
-    response.status(200).json({
-        status: 'success',
-        stats
-    })
-
-})
