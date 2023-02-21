b8b3162b9d6b23f0173d7dc4ed4a4cd1b92f9c46
HuvonHutchinson-Goodridge <huvongoodridge@gmail.com>
2023-02-21 00:15:03 -0500

    added logout button to top bar and organized imports

--- client/src/components/Topbar.js
+++ client/src/components/Topbar.js
@@ -1,19 +1,22 @@
-import { Box, IconButton, useTheme } from "@mui/material";
-import { useContext } from 'react';
-import  InputBase  from "@mui/material/InputBase"
-import { ColorModeContext, tokens } from "./../theme"
+//Color Theme Files
+import { tokens } from "./../theme"
+
+//React-Router-Dom
+import { useNavigate } from 'react-router-dom'
+
+//Material UI
+import { Box, IconButton, useTheme, Button } from "@mui/material";
+import InputBase from "@mui/material/InputBase"
 import NotificationsOutlinedIcon from "@mui/icons-material/NotificationsOutlined"
 import SettingsOutlinedIcon from "@mui/icons-material/SettingsOutlined"
 import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined"
 import SearchIcon from "@mui/icons-material/Search"
 
-const TopBar = () => {
+const TopBar = ({ LogOut }) => {
     const theme = useTheme();
     const colors = tokens(theme.palette.mode);
-    const colorMode = useContext(ColorModeContext);
-  
+    const navigate = useNavigate()
     return (<Box display="flex" justifyContent="space-between" padding={2}>
-        
         <Box display="flex" backgroundColor={colors.primary[400]} borderRadius="3px">
             <InputBase sx={{ ml: 2, flex: 1 }} placeholder="Search" />
             <IconButton type="button" sx={{ p: 1 }}>
@@ -30,8 +33,11 @@ const TopBar = () => {
             <IconButton>
                 <PersonOutlinedIcon />
             </IconButton>
+            <Button color="secondary" variant="contained" onClick={() => LogOut(navigate)}>
+                Log Out
+            </Button>
         </Box>
     </Box>)
 }
 
-export default TopBar;
+export default connect(null, { LogOut })(TopBar);
