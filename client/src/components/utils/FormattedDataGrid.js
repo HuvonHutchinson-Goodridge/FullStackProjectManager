7bc55d40d2dd2cb6837d98cc0d439c318096e380
HuvonHutchinson-Goodridge <huvongoodridge@gmail.com>
2023-03-26 22:41:49 -0400

    added fetchPage as parameter and removed unnecessary imports

--- client/src/components/utils/FormattedDataGrid.js
+++ client/src/components/utils/FormattedDataGrid.js
@@ -1,21 +1,16 @@
 //Material UI
-import { Box, Grid, Button, useTheme, Typography } from '@mui/material';
+import { Grid, useTheme} from '@mui/material';
 import { DataGrid } from '@mui/x-data-grid';
-import AdminPanelSettingsOutlinedIcon from '@mui/icons-material/AdminPanelSettingsOutlined';
-import LockOpenOutlinedIcon from '@mui/icons-material/LockOpenOutlined';
 
 //React
 import { useEffect, useState } from 'react'
 import { connect } from 'react-redux'
 
-//Formik
-import { Formik, Form } from 'formik'
-
 //Files
 import { tokens } from "./../../theme"
 import { fetchPage} from './../../store/actions'
 
-const FormattedDataGrid = ({ styles, header, subHeader, rows, columns, ...props }) => {
+const FormattedDataGrid = ({ fetchPage, styles, header, subHeader, rows, columns, ...props }) => {
     const theme = useTheme();
     const colors = tokens(theme.palette.mode);
 
