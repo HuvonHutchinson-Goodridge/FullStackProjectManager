2a7e1ad07f3c55361ffcabc1595d94f1944ab4d3
HuvonHutchinson-Goodridge <huvongoodridge@gmail.com>
2023-03-27 23:35:19 -0400

    changed the reducers to use objects instead of arrays

--- client/src/store/reducers/bugReducer.js
+++ client/src/store/reducers/bugReducer.js
@@ -1,38 +1,24 @@
-const INITIAL_STATE = [];
+const INITIAL_STATE = {};
 
 /*Reducer for all bugs in the database
  * */
 export const bugReducer = (state = INITIAL_STATE, action) => {
-        switch (action.type) {
-            case "UPDATED_BUGS":
-                state.forEach((bugsOnProject, index, array) => {
-                    if (bugsOnProject[0] !== undefined && action.payload.projectID === bugsOnProject.project) {
-                        array[index] = action.payload.bugs
-                    }
-                })
-                return [...state]
-            case "CREATED_BUGS":
-                state.forEach((bugsOnProject, index, array) => {
-                    if (bugsOnProject[0] !== undefined && action.payload.projectID === bugsOnProject.project) {
-                        array[index] = action.payload.bugs
-                    }
-                })
-                return [...state]
-            case "DELETE_BUGS":
-                state.forEach((bugsOnProject, index, array) => {
-                    if (bugsOnProject[0] !== undefined && action.payload.projectID === bugsOnProject.project) {
-                        const filtered = bugsOnProject.filter((cur, i, arr) => {
-                            return cur._id !== action.payload.id;
-                        })
-                        array[index] = filtered;
-                    }
-                    
-                })
-                return [...state]
-            case "BUGS":
-                return [...state, ...action.payload]
-            default:
-                return state;
-        }
-    
+    switch (action.type) {
+        case "UPDATED_BUGS":
+            Object.assign(state[action.payload.projectID][action.payload.bugID], action.payload.bug);
+
+            return { ...state }
+        case "CREATED_BUGS":
+
+            return { ...state, ...action.payload }
+        case "DELETE_BUGS":
+            delete state[action.payload.projectID][action.payload.bugID]
+ 
+            return { ...state }
+        case "BUGS":
+            return { ...state, ...action.payload }
+        default:
+            return state;
+    }
+
 }
