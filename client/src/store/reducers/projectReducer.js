5329d60507d7ba009ea578867a64cf0ac3cca46e
HuvonHutchinson-Goodridge <huvongoodridge@gmail.com>
2023-03-27 23:35:49 -0400

    changed reducer to use objects instead of arrays

--- client/src/store/reducers/projectReducer.js
+++ client/src/store/reducers/projectReducer.js
@@ -1,11 +1,11 @@
-const INITIAL_STATE = [];
+const INITIAL_STATE = {};
 
 /*Reducer for all projects in the database
  * */
 export const projectReducer = (state = INITIAL_STATE, action) => {
     switch (action.type) {
         case "PROJECTS":
-            return [...state, ...action.payload]
+            return { ...state, ...action.payload }
         default:
             return state;
     }
